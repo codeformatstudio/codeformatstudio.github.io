9015256a5b73ac3719de232166d995f9bf6abf44
Moaz Ahmed Hafez <moaz96526@gmail.com>
2025-11-11 16:24:08 +0400

    Update code-editor.js

--- code-editor.js
+++ code-editor.js
@@ -141,7 +141,7 @@ function generateFullOutput() {
     try {
       jsContent = ts.transpile(jsInput.value, {
         module: ts.ModuleKind.ESNext,
-        target: ts.ScriptTarget.ES2017,
+        target: ts.ScriptTarget.ESNext,
       });
     } catch (err) {
       jsContent = `console.error("TypeScript Error:", ${JSON.stringify(
