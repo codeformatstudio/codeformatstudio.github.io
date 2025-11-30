a601e050bc7d26d714a53c932539c99858fad634
Moaz Ahmed Hafez <moaz96526@gmail.com>
2025-11-30 18:36:52 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -298,11 +298,12 @@ const urlsToCache = [
 "/imageconverter.css",
 
 ];
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // forces immediate activation
+});
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
@@ -310,17 +311,5 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
-  );
-});
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (!cacheWhitelist.includes(key)) return caches.delete(key);
-        })
-      )
-    )
   );
 });
