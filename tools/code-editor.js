d5c22c10889ebed7e98912ad1179c8d83fe67428
Moaz Ahmed Hafez <moaz96526@gmail.com>
2026-06-16 13:24:27 +0400

    Update code-editor.js

--- tools/code-editor.js
+++ tools/code-editor.js
@@ -1,4 +1,85 @@
+const editorSwitch = document.getElementById("editorSelect");
 
+function switchEditor() {
+    localStorage.setItem("editor", editorSwitch.value);
+
+    location.reload(); // recreate editors using saved choice
+}
+function createMonacoEditors() {
+    const htmlValue = htmlEditor.getValue();
+    const cssValue = cssEditor.getValue();
+    const jsValue = jsEditor.getValue();
+    const pyValue = pyEditor.getValue();
+
+    htmlEditor.toTextArea();
+    cssEditor.toTextArea();
+    jsEditor.toTextArea();
+    pyEditor.toTextArea();
+
+    htmlEditor = monaco.editor.create(htmlInput.parentElement, {
+        value: htmlValue,
+        language: "html",
+        theme: "vs-dark",
+        automaticLayout: true
+    });
+
+    cssEditor = monaco.editor.create(cssInput.parentElement, {
+        value: cssValue,
+        language: "css",
+        theme: "vs-dark",
+        automaticLayout: true
+    });
+
+    jsEditor = monaco.editor.create(jsInput.parentElement, {
+        value: jsValue,
+        language: "javascript",
+        theme: "vs-dark",
+        automaticLayout: true
+    });
+
+    pyEditor = monaco.editor.create(pyInput.parentElement, {
+        value: pyValue,
+        language: "python",
+        theme: "vs-dark",
+        automaticLayout: true
+    });
+}
+function createCodeMirrorEditors() {
+    const htmlValue = htmlEditor.getValue();
+    const cssValue = cssEditor.getValue();
+    const jsValue = jsEditor.getValue();
+    const pyValue = pyEditor.getValue();
+
+    htmlEditor.dispose();
+    cssEditor.dispose();
+    jsEditor.dispose();
+    pyEditor.dispose();
+
+    htmlEditor = CodeMirror.fromTextArea(htmlInput, {
+        mode: "htmlmixed",
+        lineNumbers: true
+    });
+    htmlEditor.setValue(htmlValue);
+
+    cssEditor = CodeMirror.fromTextArea(cssInput, {
+        mode: "css",
+        lineNumbers: true
+    });
+    cssEditor.setValue(cssValue);
+
+    jsEditor = CodeMirror.fromTextArea(jsInput, {
+        mode: "javascript",
+        lineNumbers: true
+    });
+    jsEditor.setValue(jsValue);
+
+    pyEditor = CodeMirror.fromTextArea(pyInput, {
+        mode: "python",
+        lineNumbers: true
+    });
+    pyEditor.setValue(pyValue);
+}
+editorSwitch.value = localStorage.getItem("editor") || "codemirror";
 // ============================================
 // Smart Autocompletion for All Languages
 // ============================================
